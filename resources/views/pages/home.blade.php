9dab7bb177f0376e9e2a56f154c1e29c86d79ae8
joechainz <jgibson@saltstack.com>
2020-04-07 20:18:02 -0600

    spacing

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,136 +7,170 @@
         <div class="container">
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/404787912" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Devil Music</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/189410178" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">My Trip With Jer</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/42327009" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">The King Of Utah</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/251265477" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Heavy Metal Thunder</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/272859841" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Mr. Big</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/118100300" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Salty Baths</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/106253048" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">CheerStyle92</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/42324991" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Devil music. DeBaca County</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/42325148" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Devil Music. My Prelude</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/42324990" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Devil Music. Sit & Stay</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/266775018" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Nick’s Big Show. The Pitch. (Episode 1)</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/266776095" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Nick’s Big Show. Assembling The Crew. (Episode 2)</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/274776708" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Nick’s Big Show. Grass Roots. (Episode 3)</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/276078581" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Nick’s Big Show: Set It and Forget It. (Episode 4)</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/276078684" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Nick’s Big Show. The Show. (Episode 5)</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/276078929" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Nick’s Big Show: The Show Pt 2. (Episode 6)</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/201355915" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Ford Clitaurus</h3>
                 </div>
@@ -147,8 +181,10 @@
         <div class="container">
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/364098729" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Hi-Fi</h3>
                 </div>
@@ -159,96 +195,120 @@
         <div class="container">
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/266771239" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Better Traction</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/266771230" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Job Shadow</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/266771227" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">High Five</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/266771219" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Chair Wars</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/266771234" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Nicknames</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/276071659" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Tarantula</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/276268603" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Iron</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/276071026" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Blowtorch</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/276071066" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Nail</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/276071634" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Feathers</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/274711012" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Ice Fishing</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/133644700" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Space Monkey</h3>
                 </div>
@@ -259,16 +319,20 @@
         <div class="container">
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/203646660" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">BTS Ford Clitaurus</h3>
                 </div>
             </div>
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/42325853" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">BTS Devil Music</h3>
                 </div>
@@ -279,8 +343,10 @@
         <div class="container">
             <div class="row pt-5 my-5">
                 <div class="col-12">
+                    <div class="max mx-auto">
                     <div class='embed-container'>
                         <iframe src="https://player.vimeo.com/video/404786388" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <h3 class="mt-3">Logo</h3>
                 </div>
