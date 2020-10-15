65ea8c996d1c5b0618cdbb27a6b6aa815d3d7507
joechainz <jgibson@saltstack.com>
2020-10-15 09:24:06 -0600

    added stuff

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -69,20 +69,20 @@
                 <div class="col-12">
                     <div class="max mx-auto">
                     <div class='embed-container'>
-                        <iframe src="https://player.vimeo.com/video/254193306" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                        <iframe src="https://player.vimeo.com/video/272859841" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
                     </div>
                     </div>
-                    <h3 class="mt-3">Agua Donkeys. Original Short Film</h3>
+                    <h3 class="mt-3">Mr. Big (A Tribute to Rock 'n' Roll)</h3>
                 </div>
             </div>
             <div class="row my-5">
                 <div class="col-12">
                     <div class="max mx-auto">
                     <div class='embed-container'>
-                        <iframe src="https://player.vimeo.com/video/272859841" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                        <iframe src="https://player.vimeo.com/video/254193306" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
                     </div>
                     </div>
-                    <h3 class="mt-3">Mr. Big (A Tribute to Rock 'n' Roll)</h3>
+                    <h3 class="mt-3">Agua Donkeys. Original Short Film</h3>
                 </div>
             </div>
             <div class="row my-5">
