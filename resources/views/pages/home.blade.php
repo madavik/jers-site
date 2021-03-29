1991e653b40fcc5dc1a4c324f45dc128684183ed
joechainz <jgibson@saltstack.com>
2021-03-28 17:41:44 -0700

    a little death

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -213,20 +213,20 @@
                 <div class="col-12">
                     <div class="max mx-auto">
                     <div class='embed-container'>
-                        <iframe src="https://player.vimeo.com/video/364098729" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                        <iframe src="https://player.vimeo.com/video/520452429" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
                     </div>
                     </div>
-                    <h3 class="mt-3">Hi-Fi</h3>
+                    <h3 class="mt-3">A Little Death</h3>
                 </div>
             </div>
             <div class="row my-5">
                 <div class="col-12">
                     <div class="max mx-auto">
                     <div class='embed-container'>
-                        <iframe src="https://player.vimeo.com/video/520452429" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                        <iframe src="https://player.vimeo.com/video/364098729" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
                     </div>
                     </div>
-                    <h3 class="mt-3">A Little Death</h3>
+                    <h3 class="mt-3">Hi-Fi</h3>
                 </div>
             </div>
         </div>
